ec9fff65fccb27f949b09a4247e718bef9581d7e
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-07-28 21:41:34 +0300

    task 3 check

---
+++ task_3.sql
@@ -0,0 +1,15 @@
+USE alx_book_store;
+SELECT * FROM books;
+SELECT * FROM authors;
+SELECT * FROM customers;
+SELECT * FROM orders;
+SELECT * FROM order_details
+
+
+
+
+-- books
+-- authors
+-- customers
+-- orders
+-- order details
