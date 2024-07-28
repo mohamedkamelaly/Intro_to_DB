cd42dfe48c9c40990e445e48e8d457dce0a1bade
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-07-28 20:30:51 +0300

    Introduction to DB

---
+++ alx_book_store.sql
@@ -0,0 +1,39 @@
+ CREATE DATABASE alx_book_store
+ USE alx_book_store;
+ CREATE TABLE Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    Publication_data DATE,
+    Foreign Key (author_id) REFERENCES Authors(author_id)
+ )
+
+ CREATE TABLE Authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+ )
+
+ CREATE Table Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+ )
+
+ CREATE Table Orders(
+    order_id PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    Foreign Key (customer_id) REFERENCES Customers(customer_id)
+ )
+
+
+CREATE Table Order_Details(
+    orderdetail_id  PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    Foreign Key (order_id) REFERENCES Orders(order_id),
+    Foreign Key (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+)
